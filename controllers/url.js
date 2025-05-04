e4273d85cda7608202afecc916e3f7011d2bb2d0
shivagayathri <sgayathri@canyonranch.com>
2025-05-04 18:27:55 +0530

    added routes,controller and establish mongodb connection
    Signed-off-by: shivagayathri <sgayathri@canyonranch.com>

---
+++ controllers/url.js
@@ -0,0 +1,39 @@
+const { nanoid }= require('nanoid')
+const URL = require('../models/url');
+
+
+async function handleGenerateShortUrl(req, res) {
+    const body = req.body;
+    if (!body.url) {
+        return res.status(400).json({
+        error:'url is required'
+    })
+    }
+    const shortID = nanoid(8) 
+    await URL.create({
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistory:[]
+    })
+
+    return res.json({ id: shortID });
+}
+
+async function handleGetAnalytics(req,res) {
+    const shortId = req.params.shortId
+
+    if (!shortId) {
+        return res.status(400).json({
+            error: 'shortId is required'
+        })
+    }
+    const result = await URL.findOne({ shortId })
+     return res.json(`total click:${result.visitHistory.length}`)
+}
+
+module.exports = {
+    handleGenerateShortUrl,
+    handleGetAnalytics
+}
+
+
